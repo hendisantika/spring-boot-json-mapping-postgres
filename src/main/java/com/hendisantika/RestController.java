be31099d8e94193a5703756518603f50fd15aed0
Hendi Santika <hendisantika@gmail.com>
2021-10-28 17:47:01 +0700

    #22 - Add parent data in RestController

--- src/main/java/com/hendisantika/RestController.java
+++ src/main/java/com/hendisantika/RestController.java
@@ -1,9 +1,15 @@
 package com.hendisantika;
 
+import com.hendisantika.entity.Bio;
+import com.hendisantika.entity.Child;
+import com.hendisantika.entity.Parent;
+import com.hendisantika.repository.ParentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import static java.util.Arrays.asList;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,6 +25,9 @@ public class RestController {
     @Autowired
     private MyTableRepository repository;
 
+    @Autowired
+    private ParentRepository parentRepository;
+
     @GetMapping("/getData")
     public Iterable<MyTable> getData() {
         return repository.findAll();
@@ -28,5 +37,16 @@ public class RestController {
     public void addData(@RequestBody MyTable tableData) {
         repository.save(tableData);
     }
+
+    @GetMapping("/addData2")
+    public void addData2() {
+        parentRepository.save(new Parent(
+                        "parent1",
+                        asList(new Child("child1"), new Child("child2")),
+                        new Bio("bio1")
+                )
+        );
+    }
+
 
 }
