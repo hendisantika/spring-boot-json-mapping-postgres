4b30f399afe392adb567937f73bd65906e91fd11
Hendi Santika <hendisantika@gmail.com>
2021-10-28 17:52:08 +0700

    #23 - Add get parent data in RestController

--- src/main/java/com/hendisantika/RestController.java
+++ src/main/java/com/hendisantika/RestController.java
@@ -4,10 +4,14 @@ import com.hendisantika.entity.Bio;
 import com.hendisantika.entity.Child;
 import com.hendisantika.entity.Parent;
 import com.hendisantika.repository.ParentRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 import static java.util.Arrays.asList;
 
@@ -20,6 +24,7 @@ import static java.util.Arrays.asList;
  * Date: 28/10/21
  * Time: 16.44
  */
+@Log4j2
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     @Autowired
@@ -33,6 +38,17 @@ public class RestController {
         return repository.findAll();
     }
 
+    @GetMapping("/getData2/{id}")
+    public Parent getParentData(@PathVariable("id") Integer id) {
+        Parent result = parentRepository.findById(id).orElseThrow();
+        List<Child> children = result.getChildren();
+        Bio bio = result.getBio();
+        log.info("Data 1: ", result);
+        log.info("Data 2: ", children);
+        log.info("Data 3: ", bio);
+        return result;
+    }
+
     @PostMapping("/addData")
     public void addData(@RequestBody MyTable tableData) {
         repository.save(tableData);
@@ -41,7 +57,7 @@ public class RestController {
     @GetMapping("/addData2")
     public void addData2() {
         parentRepository.save(new Parent(
-                        "parent1",
+                "parent1",
                         asList(new Child("child1"), new Child("child2")),
                         new Bio("bio1")
                 )
